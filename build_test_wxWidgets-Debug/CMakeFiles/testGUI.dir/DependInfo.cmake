b889618baeb54fd35a7dd1482ad6efc307b895a8
Danil Zablotsky <zibliclubofficial@gmail.com>
2023-03-09 01:36:48 +0600

    Update

---
+++ build_test_wxWidgets-Debug/CMakeFiles/testGUI.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/prog/cpp/GitHub/test_wxWidgets/src/app/app.cpp" "CMakeFiles/testGUI.dir/src/app/app.cpp.obj" "gcc" "CMakeFiles/testGUI.dir/src/app/app.cpp.obj.d"
+  "D:/prog/cpp/GitHub/test_wxWidgets/src/main_frame/main_frame.cpp" "CMakeFiles/testGUI.dir/src/main_frame/main_frame.cpp.obj" "gcc" "CMakeFiles/testGUI.dir/src/main_frame/main_frame.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_test_wxWidgets-Debug/CMakeFiles/testGUI.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGUI.dir/src/app/app.cpp.obj"
+  "CMakeFiles/testGUI.dir/src/app/app.cpp.obj.d"
+  "CMakeFiles/testGUI.dir/src/main_frame/main_frame.cpp.obj"
+  "CMakeFiles/testGUI.dir/src/main_frame/main_frame.cpp.obj.d"
+  "libtestGUI.dll.a"
+  "testGUI.exe"
+  "testGUI.exe.manifest"
+  "testGUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
