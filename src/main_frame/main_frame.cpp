8297c981cc4d6367cb935b04eedd427f868eba30
Danil Zablotsky <115803050+zibliclub@users.noreply.github.com>
2023-03-09 18:53:08 +0600

    Update

--- src/main_frame/main_frame.cpp
+++ src/main_frame/main_frame.cpp
@@ -1,4 +1,5 @@
 #include "main_frame.h"
+#include <wx/icon.h>
 
 enum
 {
@@ -13,6 +14,8 @@ END_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString &title) : wxFrame(nullptr, wxID_ANY, title)
 {
+    SetIcon(wxICON(icon_frame));
+
     wxSize *size = new wxSize(800, 600);
     this->SetMinClientSize(*size);
     this->Center();
@@ -20,15 +23,47 @@ MainFrame::MainFrame(const wxString &title) : wxFrame(nullptr, wxID_ANY, title)
     wxMenu *menuFile = new wxMenu;
     wxMenu *menuNewMatrix = new wxMenu;
     menuFile->Append(wxID_ANY, "New Matrix...\t Ctrl+N", menuNewMatrix, "Open the file with the matrix or create a new one");
-    menuNewMatrix->Append(ID_Create_Matrix, "Create Matrix", "Create a new matrix (fill manually or with random values)");
     menuNewMatrix->Append(ID_Open_From_A_File, "Open from a file", "Open the file with the matrix (binary or typed)");
+    menuNewMatrix->Append(ID_Create_Matrix, "Create Matrix", "Create a new matrix (fill manually or with random values)");
 
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append(menuFile, "&File");
 
     SetMenuBar(menuBar);
 
-    wxPanel *panel = new wxPanel(this);
+    wxPanel *panel_left = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 500));
+    panel_left->SetBackgroundColour(wxColor(240, 244, 245));
+
+    wxFont buttonFont(12, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+    wxFont bigTextFont(20, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+
+    wxButton *button_open = new wxButton(panel_left, ID_Open_From_A_File, wxT("Открыть"), wxPoint(-1, 50), wxSize(130, 40), wxBORDER_THEME);
+    wxButton *button_create = new wxButton(panel_left, ID_Create_Matrix, wxT("Создать"), wxPoint(-1, 120), wxSize(130, 40), wxBORDER_THEME);
+
+    button_open->Bind(wxEVT_BUTTON, MainFrame::OpenFile, this);
+    button_open->SetBackgroundColour(*wxWHITE);
+    button_open->SetFont(buttonFont);
+    button_open->Center(wxHORIZONTAL);
+
+    button_create->SetBackgroundColour(*wxWHITE);
+    button_create->SetFont(buttonFont);
+    button_create->Center(wxHORIZONTAL);
+
+    wxPanel *panel_right = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(800, 500));
+    panel_right->SetBackgroundColour(wxColor(*wxWHITE));
+
+    wxStaticText *bigText = new wxStaticText(panel_right, wxID_ANY, wxT("Индивидуальное задание №5"), wxPoint(20, 20));
+    wxStaticText *smallText = new wxStaticText(panel_right, wxID_ANY,
+        wxT("Отсортировать строки матрицы по неубыванию абсолютных величин сумм их элементов. Сортировка бинарными включениями."),
+        wxPoint(20, 70));
+
+    bigText->SetFont(bigTextFont);
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(panel_left, 0, wxEXPAND);
+    sizer->Add(panel_right, 1, wxEXPAND);
+
+    this->SetSizerAndFit(sizer);
 
     CreateStatusBar();
 }
---
+++ src/resource/resource.h
@@ -0,0 +1,6 @@
+#ifdef RESOURCE_H
+#define RESOURCE_H
+
+#define icon_frame
+
+#endif
